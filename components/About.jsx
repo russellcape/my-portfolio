4d7fa8775791eecada1c599b0a686e90ff3f4ccc
russellcape <russellcape98@gmail.com>
2023-05-13 19:41:38 -0400

    worked on about text

--- components/About.jsx
+++ components/About.jsx
@@ -9,16 +9,16 @@ const About = () => {
           <p>
             As a developer, I have always been passionate about creating elegant and effective solutions to
             complex problems. I have a strong foundation in software development, with a focus on web
-            technologies such as HTML, CSS, and JavaScript. I enjoy working on both the front-end and
+            technologies such as Python and JavaScript. I enjoy working on both the front-end and
             back-end of applications, and I am always looking for ways to optimize performance, improve user
             experience, and ensure the highest level of code quality.
           </p>
           <p>
             Throughout my career, I have worked on a wide range of projects, from simple static websites to
             complex enterprise-level applications. I have experience with multiple languages such as JavaScript 
-            and Python and I am versed with a variety of development tools and frameworks, including Django, React, 
-            Next.js, Node.js and Ruby on Rails. I am always eager to learn and explore new technologies, and I am 
-            constantly seeking out opportunities to improve my skills and knowledge.
+            and Python and I am versed with a variety of development tools and frameworks, including Django Rest 
+            Framework, React, Next.js, Node.js and Docker. I am always eager to learn and explore new technologies, 
+            and I am constantly seeking out opportunities to improve my skills and knowledge.
           </p>
         </div>
         <div className="about-img">
