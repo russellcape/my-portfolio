450a260226ff35b0cd4e1a8d8e15384e569e36d3
russellcape <russellcape98@gmail.com>
2023-05-13 19:56:40 -0400

    added project data and the project card component

---
+++ components/ProjectData.js
@@ -0,0 +1,37 @@
+export const projectData = [
+    {
+      id: 1,
+      title: 'Project #1',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam convallis malesuada dui ut ullamcorper. Cras tempor, diam at porttitor auctor, metus.',
+      githubLink: 'https://github.com/russellcape',
+    },
+    {
+      id: 2,
+      title: 'Project #2',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam convallis malesuada dui ut ullamcorper. Cras tempor, diam at porttitor auctor, metus.',
+      githubLink: 'https://github.com/russellcape',
+    },
+    {
+      id: 3,
+      title: 'Project #3',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam convallis malesuada dui ut ullamcorper. Cras tempor, diam at porttitor auctor, metus.',
+      githubLink: 'https://github.com/russellcape',
+    },
+    {
+      id: 4,
+      title: 'Project #4',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam convallis malesuada dui ut ullamcorper. Cras tempor, diam at porttitor auctor, metus.',
+      githubLink: 'https://github.com/russellcape',
+    },
+    {
+      id: 5,
+      title: 'Project #5',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam convallis malesuada dui ut ullamcorper. Cras tempor, diam at porttitor auctor, metus.',
+      githubLink: 'https://github.com/russellcape',
+    },
+  ];
