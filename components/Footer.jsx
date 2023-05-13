77101e8b789a3470381f54dcbb4686ad439a865e
russellcape <russellcape98@gmail.com>
2023-05-13 19:04:34 -0400

    created basic footer component

---
+++ components/Footer.jsx
@@ -0,0 +1,40 @@
+const Footer = () => {
+    return (
+      <>
+        <hr/>
+        <div className="footer-container">
+          <p>
+            © {new Date().getFullYear()} Russell's Portfolio
+          </p>
+          <div className="social_icons">
+            <a
+              href="https://twitter.com/"
+              aria-label="Twitter"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fa-brands fa-twitter"></i>
+            </a>
+            <a
+              href="https://github.com/russellcape/"
+              aria-label="GitHub"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fa-brands fa-github"></i>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/russellcape/"
+              aria-label="LinkedIn"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+          </div>
+        </div>
+      </>
+    )
+  }
+  
+  export default Footer;
